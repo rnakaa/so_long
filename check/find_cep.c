64767ab90b03d876fa8bfeae495246a5a3441114
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-03-23 18:13:09 +0900

    map check done

--- check/find_cep.c
+++ check/find_cep.c
@@ -6,25 +6,35 @@
 /*   By: rnaka <rnaka@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 17:35:45 by rnaka             #+#    #+#             */
-/*   Updated: 2023/03/23 04:09:26 by rnaka            ###   ########.fr       */
+/*   Updated: 2023/03/23 18:04:51 by rnaka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 #include"so_long.h"
 
-int	find_cep(int i, int j, char **map, int *list)
+static int	find_c(int i, int j, char **map, int *list)
 {
-	ft_printf("%c i=%d j=%d \n", map[i][j], i, j);
-	int k = 0;
-	while (k < maplen(map[0])*mapsize(map))
-	{
-		ft_printf("%d",list[k]);
-		if (k%maplen(map[0]) == 0)
-			ft_printf("\n");
-		k++;
-	}
-	ft_printf("\n");
+	if (list[i*maplen(map[0])+j])
+		return (0);
+	list[i*maplen(map[0])+j] = 1;
+	if(map[i][j] == 'C')
+		return (1);
+	else if (map[i][j] == '1' || map[i][j] == 'E')
+		return (0);
+	else if (find_c(i + 1, j, map, list))
+		return (1);
+	else if (find_c(i, j + 1, map, list))
+		return (1);
+	else if (find_c(i - 1, j, map, list))
+		return (1);
+	else if (find_c(i, j - 1, map, list))
+		return (1);
+	return (0);
+}
+
+static int	find_e(int i, int j, char **map, int *list)
+{
 	if (list[i*maplen(map[0])+j])
 		return (0);
 	list[i*maplen(map[0])+j] = 1;
@@ -32,13 +42,65 @@ int	find_cep(int i, int j, char **map, int *list)
 		return (1);
 	else if (map[i][j] == '1')
 		return (0);
-	else if (find_cep(i + 1, j, map, list))
+	else if (find_e(i + 1, j, map, list))
 		return (1);
-	else if (find_cep(i, j + 1, map, list))
+	else if (find_e(i, j + 1, map, list))
 		return (1);
-	else if (find_cep(i - 1, j, map, list))
+	else if (find_e(i - 1, j, map, list))
 		return (1);
-	else if (find_cep(i, j - 1, map, list))
+	else if (find_e(i, j - 1, map, list))
 		return (1);
 	return (0);
+}
+
+static void	find_p(int *i, int *j, char **map)
+{
+	int	h;
+	int	b;
+
+	h = 0;
+	b = 0;
+	while (map[h])
+	{
+		b = 0;
+		while (map[h][b])
+		{
+			if (map[h][b] == 'P')
+			{
+				*i = h;
+				*j = b;
+			}
+			b++;
+		}
+		h++;
+	}
+}
+
+static void	reset_list(char **map, int *list)
+{
+	int	i;
+
+	i = 0;
+	while (i < maplen(map[0]) * mapsize(map))
+	{
+		list[i] = 0;
+		i++;
+	}
+}
+
+void	find_cep(char **map)
+{
+	int	i;
+	int	j;
+	int	list[maplen(map[0]) * mapsize(map)];
+
+	i = 0;
+	j = 0;
+	reset_list(map, list);
+	find_p(&i, &j, map);
+	if (!find_e(i, j, map, list))
+		error_free(map, 8);
+	reset_list(map, list);
+	if (!find_c(i, j, map, list))
+		error_free(map, 8);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rnaka <rnaka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 17:20:24 by rnaka             #+#    #+#             */
-/*   Updated: 2023/03/22 19:01:27 by rnaka            ###   ########.fr       */
+/*   Updated: 2023/03/23 08:59:15 by rnaka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,39 +25,7 @@ int	main(int argc, const char *argv[])
 		error(2);
 	map = const_map(fd);
 	check_map(map);
-
-
-	int i = 0;
-	int j = 0;
-	int ii = 0;
-	int jj = 0;
-
-	while(map[i])
-	{
-		j = 0;
-		while(map[i][j])
-		{
-			if (map[i][j] == 'P')
-			{
-				ii = i;
-				jj = j;
-			}
-			j++;
-		}
-		i++;
-	}
-	ft_printf("%d  %d\n", ii, jj);
-	int list[maplen(map[0])*mapsize(map)];
-	list[maplen(map[0])*mapsize(map) - 1] = 2;
-	int nn = 0;
-	list[0] = 0;
-	while (nn < maplen(map[0])*mapsize(map)) 
-	{
-		list[nn] = 0;
-		nn++;
-	}
-	if (!find_cep(ii, jj, map, list))
-		error(8);
+	find_cep(map);
 	free_dp_char(map);
 	return (0);
 }
