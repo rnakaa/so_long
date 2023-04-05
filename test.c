65a5df13c48c17cfd9d4713b49ab1108018a59b1
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-04-05 19:25:19 +0900

    nana

---
+++ test.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnaka <rnaka@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 11:42:34 by rnaka             #+#    #+#             */
+/*   Updated: 2023/04/04 12:00:08 by rnaka            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int	main()
+{
+	system("./so_long b1.ber");
+	sleep(3);
+	system("./so_long b2.ber");
+	sleep(3);
+	system("./so_long b3.ber");
+	sleep(3);
+	system("./so_long b4.ber");
+	sleep(3);
+	system("./so_long b5.ber");
+	sleep(3);
+	system("./so_long b6.ber");
+	sleep(3);
+	system("./so_long b7.ber");
+	sleep(3);
+	system("./so_long b8.ber");
+}
